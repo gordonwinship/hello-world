f09fe254707912e8f003ec723b84a7fb531105ac
Felix Borchers <felix@email.com>
2019-02-01 18:08:24 +0100

    Fibonacci Print Change

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,13 @@
 '''Fibonacci Numbers'''
 def fibonacci(n, printall=False):
+    print("-"*10)
     f = 0
     f2 = 1
     for i in range(n):
         f, f2 = f2, f2+1
         if printall:
             print(f)
-    print(f)
+    print("F_{} = {}".format(n,f))
+    print("-"*10)
 
 fibonacci(2)
